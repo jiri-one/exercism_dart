d4eb9e57152e976e1d81e54b755781d90c6a8bee
jiri.one <nemec@jiri.one>
2022-05-28 19:55:18 +0200

    better solution for word-count

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,25 +1,21 @@
 class WordCount {
-    Map<String, int> countWords(String sentence) {
-        sentence = sentence.replaceAll("\n", " ");
-        sentence = sentence.replaceAll(",", " ");
-        RegExp exp = RegExp(r"[^:!&@\$%^&.]");
-        //RegExp exp = RegExp(r"[a-zA-Z]|'|\d|\s");
-        final clearedList =
-            exp.allMatches(sentence).map((element) => element.group(0)).toList();
-        var map = <String, int>{};
-        for (var word in clearedList.join("").split(" ")) {
-        word = word.toLowerCase().trim();
-        word = word.replaceAll(RegExp(r"(\')$|^(\')"), "");
-        if (word.isNotEmpty) {
-            int wordCount = map[word] ?? 0;
-            if (wordCount == 0) {
-            map[word] = 1;
-            } else {
-            wordCount++;
-            map[word] = wordCount;
-            }
-        }
-        }
-        return map;
+  Map<String, int> countWords(String input) {
+    input = input
+        .replaceAll("\n", " ")
+        .replaceAll(",", " ")
+        .replaceAll(RegExp(r"[:!&@\$%^&.]"), "");
+
+    var splitedInput = input.split(" ");
+    var countMap = <String, int>{};
+    splitedInput.removeWhere((item) => item.isEmpty); // remove empty items
+    splitedInput =
+        splitedInput.map((item) => item.toLowerCase().trim()).toList();
+    splitedInput = splitedInput
+        .map((item) => item.replaceAll(RegExp(r"(\')$|^(\')"), ""))
+        .toList();
+    for (var word in splitedInput) {
+      countMap[word] = (countMap[word] ?? 0) + 1;
     }
+    return countMap;
+  }
 }
